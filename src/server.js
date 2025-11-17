fe808c7fd3544e772a3a796c2830954287a8a439
Brian Benson <brianbenson317@gmail.com>
2025-11-17 17:56:54 +0300

    Wetube_Lecture
    on middleware, export and Routes

--- src/server.js
+++ src/server.js
@@ -1,43 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method}: ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not allowed</h1>");
-  }
-  console.log("Allowed, you may continue")
-  next();
-};
-  
-const handleHome = (req, res) => {
-  return res.send("In the middleware");
-};
-
-const handleLogin = (req, res) => {
-  return res.send("Login here ");
-};
-
-const handleRegister = (req, res) => {
-  return res.send("Register here");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lodge")
-}
-
-// app.use();
-app.get("/", logger, handleHome);
-app.get("/login", handleLogin);
-app.get("/register", handleRegister);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✔️ server listening on port http://localhost:${PORT} 🚀`);

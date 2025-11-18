745bc8197d8ffc9024685b7daf66f3fb260f2b29
Brian Benson <brianbenson317@gmail.com>
2025-11-18 18:31:45 +0300

    Wetube - lecture

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,9 @@
-export const trending = (req, res) => res.send("Home Page Video");
-export const see = (req, res) => res.send("watch")
-export const edit = (req, res) => res.send("Edit")
-export const search = (req, res) => res.send("search")
-export const upload = (req, res) => res.send("upload")
-export const deleteVideo = (req, res) => res.send("Delete Video")
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
+
+export const search = (req, res) => res.send("search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  return res.send(`Delete Video #${req.params.id}`);
+};
--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
+app.set("views", process.cwd() + "/src/views")
+app.set("view engine", "pug")
 app.use(logger);
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);

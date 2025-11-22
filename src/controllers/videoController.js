c953814e57c6281f731353f4e49c20e5b66397d9
Brian Benson <brianbenson317@gmail.com>
2025-11-22 14:06:48 +0300

    WEtube Lecture

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,8 +2,7 @@ import Video from "../models/Video";
 import { edit } from "./userController";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
-  // console.log(videos);
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -20,7 +19,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.render(" 404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
@@ -28,19 +27,17 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags)
+  });
   return res.redirect(`/videos/${id}`);
 };
-
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
@@ -51,11 +48,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
-      meta: {
-        views: 0,
-        rating: 0
-      }
+      hashtags: Video.formatHashtags(hashtags)
     });
     return res.redirect("/");
   } catch (error) {
@@ -64,4 +57,17 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  //delete Video
+  return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  console.log("Should Search for", keyword)
+  return res.render("search", { pageTitle: "Search" });
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -4,11 +4,17 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
   description: { type: String, required: true, trim: true },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String, trim: true}],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true }
   }
+});
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
